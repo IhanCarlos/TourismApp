a03369b66a57bab45f9a857771d3ca5a08d15cf4
ihan <ihancarlosihan@gmail.com>
2022-11-14 15:08:59 -0300

    Initial Commit

---
+++ SwiftuiMapApp/SwiftuiMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiMapAppApp.swift
+//  SwiftuiMapApp
+//
+//  Created by ihan carlos on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
